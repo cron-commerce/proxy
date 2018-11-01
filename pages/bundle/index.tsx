7557e55896a97c0050dd657b7f66891ed9e41331
Ethan Jon <e10jc@users.noreply.github.com>
2018-11-01 13:40:49 -0700

    working on a bundle builder

--- pages/bundle/index.tsx
+++ pages/bundle/index.tsx
@@ -7,6 +7,23 @@ const QUERY = gql`
   query subscribable($handle: String!) {
     subscribable(handle: $handle) {
       id
+      products {
+        id
+        shopifyProduct {
+          id
+          title
+          variants {
+            id
+            price
+            title
+          }
+        }
+      }
+      sizes {
+        id
+        numVariants
+        price
+      }
     }
   }
 `
@@ -26,7 +43,39 @@ export default class BundlePage extends Component<Props> {
       {({data, loading, error}) => {
         if (error) { return <div>error</div> }
         if (loading) { return <div>loading...</div> }
-        return <div>bundle</div>
+        console.log(data.subscribable)
+
+        return <div>
+          <h1>Bundle Builder</h1>
+
+          <h2>Select your frequency</h2>
+          <input type='number' name='freqNum' />
+          <select name='freqUnit'>
+            {['days', 'weeks', 'months'].map(unit => <option key={unit} value={unit}>{unit}</option>)}
+          </select>
+
+          <h2>Select your size</h2>
+          <select name='size'>
+            {data.subscribable.sizes.map(size => <option key={size.id} value={size.numVariants}>
+              {size.numVariants} for {size.price}
+            </option>)}
+          </select>
+
+          <h2>Select your variants</h2>
+          {data.subscribable.products.map(product => <div key={product.id}>
+            <h4>{product.shopifyProduct.title}</h4>
+            {product.shopifyProduct.variants.map(variant => <div key={variant.id}>
+              <span>{variant.title}</span>
+              <select>
+                {new Array(13).fill(undefined).map((_, i) => <option key={i} value={i}>{i}</option>)}
+              </select>
+            </div>)}
+          </div>)}
+
+          <div>
+            <button type='submit'>Add to cart</button>
+          </div>
+        </div>
       }}
     </Query>
   }
